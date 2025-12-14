85c3b3ed5ce4983fb44a093bc120c51222c4b3c0
Enjeck C. <patrathewhiz@gmail.com>
2025-12-14 10:11:51 +0000

    Updates

---
+++ _next/static/4mI37YWM-G9Y1bdPraCyX/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Blobby//_next/:path+",
+        "destination": "/Blobby/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
